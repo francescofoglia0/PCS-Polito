d0e56e9eabd99b5bef3c3f54326f49c75e8b363b
Francesco Foglia <francescofoglia04@gmail.com>
2026-03-11 16:02:45 +0000

    Consegna esercitazione1

---
+++ esercitazione1/esercizio1.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main(int argc,const char *argv[])
+{
+    // cout << "Inserisci il nome del file da leggere: \n";
+    string nomefile = argv[1];
+    // cin >> nomefile;
+    ifstream file(nomefile);
+    if (file.is_open())
+    {
+        while(!file.eof())
+        {
+            string citta;
+            double temp0, temp6, temp12, temp18;
+            file >> citta >> temp0 >> temp6 >> temp12 >> temp18;
+            if (citta == "") // se il file è finito, citta sarà una stringa vuota, quindi usciamo dal ciclo
+                break;       // altrimenti, stamperemmo una riga in piu errata
+            double media = (temp0 + temp6 + temp12 +temp18)/4;
+            cout << citta << " " << media << "\n";
+        }
+    }
+    else
+    {
+        cerr << "Impossibile aprire il file " << nomefile << "\n";
+        return 1;
+    }
+    return 0;
+}
