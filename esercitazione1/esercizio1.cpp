744d7c677db2f8bcc24b8102794d08afbc70bce5
Francesco Foglia <francescofoglia04@gmail.com>
2026-03-15 10:37:19 +0000

    aggiunta la condizione di errore se non viene specificato il parametro da terminale

--- esercitazione1/esercizio1.cpp
+++ esercitazione1/esercizio1.cpp
@@ -1,10 +1,17 @@
 #include <iostream>
 #include <fstream>
+#include <sstream>
 using namespace std;
 
 int main(int argc,const char *argv[])
 {
+    if (argc < 2)  //verifico che sia stato passato il parametro da terminale
+    {
+        cerr << "Non hai inserito il nome del file da leggere\n"; 
+        return 1;
+    }
     string nomefile = argv[1];
+    
     ifstream file(nomefile);
     if (file.is_open())
     {
@@ -14,7 +21,7 @@ int main(int argc,const char *argv[])
             double temp0, temp6, temp12, temp18;
             file >> citta >> temp0 >> temp6 >> temp12 >> temp18;
             if (citta == "") // se il file è finito, citta sarà una stringa vuota, quindi usciamo dal ciclo
-                break;       // altrimenti, stamperemmo una riga in piu errata
+                break;      // altrimenti, stamperemmo una riga in piu errata    
             double media = (temp0 + temp6 + temp12 +temp18)/4;
             cout << citta << " " << media << "\n";
         }
