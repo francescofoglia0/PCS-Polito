5c084ea39f42eaffcdbb447b7e9299cf050cf6ff
Francesco Foglia <francescofoglia04@gmail.com>
2026-05-06 17:30:22 +0000

    Consegna esercitazione 6

---
+++ esercitazione6/grafi.hpp
@@ -0,0 +1,163 @@
+#pragma once
+#include <list>
+#include <map>
+#include <set>
+#include <unordered_map>
+#include <iostream>
+
+template<typename T>
+class unidirected_edge
+{
+    private:
+        T primo;
+        T secondo;
+    public:
+        unidirected_edge() = default;
+        unidirected_edge(const T& a, const T& b)
+        {
+            if(a>b)
+            {
+                primo = b; secondo = a;
+            }
+            else
+            {
+                primo = a; secondo= b;
+            }
+        }
+
+        T from() const {return primo;}
+        T to() const {return secondo;}
+
+        bool operator<(const unidirected_edge<T>& other) const
+        {
+            // sintassi con this e other, i.e se a<b: a lo ho gia ed è (primo,secondo), other sarebbe b
+            // se i primi nodi sono diversi allora sara maggiore quello col primo nodo piu grande
+            if(primo != other.primo)
+            {
+                return primo<other.primo;
+            }
+            // se i primi nodi sono uguali confronto il secondo to
+            return secondo<other.secondo;
+        }   
+        bool operator==(const unidirected_edge<T>& other) const
+        {
+            // sintassi this e other
+            if(primo == other.primo)
+            {
+                if(secondo == other.secondo) return true;
+                else return false;
+                
+            }
+            else return false;
+        }   
+
+
+};
+
+// se mettessimo dentro la classe unidirected_edge prenderebbe come elemento di sinistra un tipo arco
+template<typename T>
+std::ostream&
+operator<<(std::ostream& os, const unidirected_edge<T>& a)
+{
+    os << "(" << a.from() << "," << a.to() << ")";
+    return os;
+}
+
+
+using namespace std;
+template<typename T>
+class unidirected_graph
+{
+    private:
+        set<unidirected_edge<T>> archi; //per alledges(), set(archi)
+        set<T> nodi;    //per allnodes(), set(nodi)
+        map<T,set<T>> nadiacenti; //per neighours(), chiave è nodo
+        map<unidirected_edge<T>,int>  ednum;//per edge_number(), uso map perche vengono ordinati con < (creata su)
+        unordered_map<int,unidirected_edge<T>> edat; //per edgeat(), per la ricerca ha O(1)
+        int contatore = 0;
+    public:
+        unidirected_graph()=default;
+        unidirected_graph(const unidirected_graph& other)
+        {
+            archi = other.archi;
+            nodi = other.nodi;
+            nadiacenti = other.nadiacenti;
+            ednum = other.ednum;
+            edat = other.edat;
+            contatore = other.contatore;
+        }
+
+        set<T> neighours(const T& nodo) const
+        {
+            //controllo se nodo è nelle chiavi e nel caso lo restituisco (O(logN))
+            if(nadiacenti.contains(nodo))
+            return nadiacenti.at(nodo);
+            else return set<T>();
+        }
+                     //passo sempre per riferimento ma come const cosi non posso modificare involontariamente
+        void add_edge(const unidirected_edge<T>& newarc)
+        {
+            // insert su un set restituisce una coppia: iteratore e vero se l'ho inserito, 
+            // falso se c'era già o altrimenti, per cui se l'ho inserito aggiungo il resto
+            auto prova = archi.insert(newarc);
+            if(prova.second)
+            {
+                nodi.insert(newarc.from());
+                nodi.insert(newarc.to());
+                nadiacenti[newarc.from()].insert(newarc.to());
+                nadiacenti[newarc.to()].insert(newarc.from());
+                ednum[newarc] = contatore;
+                edat.insert({contatore,newarc});
+                contatore++;
+            }
+        }
+
+        set<unidirected_edge<T>> all_edges() const {return archi;}
+        set<T> all_nodes() const {return nodi;}
+
+        int edge_number(const unidirected_edge<T>& arco) const
+        {//controllo se l'arco è contenuto nel grafo (chiave di ednum) e nel caso ritorno la sua
+         // numerazione, altrimenti -1
+            if(ednum.contains(arco)) return ednum.at(arco);
+            else return -1;
+        }
+        unidirected_edge<T> edge_at(int num) const
+        {//se non trova num lancia in automatico errore
+            return edat.at(num);
+        }
+
+        unidirected_graph<T> operator-(const unidirected_graph<T>& other) const
+        {
+            unidirected_graph<T> risultato;
+            int n = archi.size();
+            for(int i=0; i<n; i++)
+            {//controllo se archi di other non contiene l'arco di this, in quel caso aggiungo
+             // quell'arco in risultato
+                if(!other.archi.contains(edat.at(i)))
+                {
+                   risultato.add_edge(edat.at(i));
+                }
+            }
+            return risultato;
+        }
+
+};
+
+
+//creo un altro overload di << per stampare un grafo, ovvero i suoi archi uno dopo l'altro
+template<typename T>
+std::ostream&
+operator<<(std::ostream& os, const unidirected_graph<T>& g)
+{
+    int n = g.all_edges().size();
+    os<<"{";
+    for(int i = 0; i<n;i++)
+    {
+        os << g.edge_at(i);
+        if(i!=n-1) os << ", "; //evito di stampare la virgola dopo l'ultimo arco
+    }
+    os << "}\n";
+    return os;
+}
+
+
---
+++ esercitazione6/main.cpp
@@ -0,0 +1,71 @@
+#include "grafi.hpp"
+#include <iostream>
+
+using namespace std;
+int main()
+{
+    // unidirected_edge<int> arco1(1,3);
+    // unidirected_edge<int> arco2(2,60);
+    // unidirected_edge<int> arco3(1,10);
+    // unidirected_graph<int> g1;
+    // g1.add_edge(arco1);
+    // g1.add_edge(arco2);
+    // g1.add_edge(arco3);
+    // unidirected_graph<int> g2(g1);
+    // cout <<g2;
+
+    //oltre al file test_grafi.cpp faccio dei test da visualizzare a schermo
+    cout << "1 Test Inserimento e Duplicati" << endl;
+    unidirected_graph<int> g1;
+    g1.add_edge({1, 3});
+    g1.add_edge({3, 1});  // è uguale al primo arco, non deve essere aggiunto due volte
+    g1.add_edge({2, 60});
+    g1.add_edge({3, 2});
+
+    cout << "Grafo G1 (dovrebbe avere 3 archi): " << g1 << endl; //ho creato overload << per grafi
+
+    cout << "2 Verifica Nodi e Vicini" << endl;
+    cout << "Nodi presenti nel grafo: ";
+    //itero direttamente sugli elementi senza passare dagli indici
+    for(auto n : g1.all_nodes()) {cout << n << " ";}
+    cout << endl;
+
+    int nodo_test = 3;
+    cout << "Vicini del nodo " << nodo_test << ": ";
+    auto vicini = g1.neighours(nodo_test);
+    for(auto v : vicini) cout << v << " ";
+    cout << "\n" << endl;
+
+
+    cout << "3 Verifica Numerazione Arco Invertito" << endl;
+    unidirected_edge<int> e_cerca(60, 2); // Lo cerco invertito
+    int id = g1.edge_number(e_cerca);
+    
+    cout << "Cerco la numerazione corrispondente all'arco (60,2): " << endl;
+    if(id != -1) {
+        cout << "Numero trovato: " << id << endl;
+        cout << "Verifica inversa: l'arco al numero:  " << id << " e' " << g1.edge_at(id) << endl;
+    } else {
+        cout << "Errore: arco non trovato!" << endl;
+    }
+    cout << endl;
+
+    cout << "4 Test Operatore Sottrazione (G1 - G2)" << endl;
+    unidirected_graph<int> g2;
+    g2.add_edge({1, 3}); // Arco comune a G1
+    
+    cout << "G1: " << g1;
+    cout << "G2: " << g2;
+    
+    unidirected_graph<int> g_risultato = g1 - g2;
+    cout << "Risultato G1 - G2 (dovrebbe mancare l'arco (1,3)): " << g_risultato << endl;
+
+    //testo le stringhe
+    cout << "5 Test Genericità (Stringhe)" << endl;
+    unidirected_graph<string> g_str;
+    g_str.add_edge({"Torino", "Milano"});
+    g_str.add_edge({"Milano", "Roma"});
+    cout << "Grafo di città: " << g_str << endl;
+
+    return 0;
+}
